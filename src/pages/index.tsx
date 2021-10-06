49a54241aa59ff6c8e58c7a34c914fe22f162523
Hyunsu Joo <hs9880@gmail.com>
2021-10-06 23:49:02 +0900

    fixing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ cccccaaa
                 return <PostPreview post={post} key={post.slug} />;
             })};;
 
-            <p>
+            <p>;;
                 <Link to="/page-2/">Go to page 2</Link> <br />
                 <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
                 <Link to="/hello-world/">hello world</Link>
