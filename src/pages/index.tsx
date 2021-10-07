ba58ecfa50b6613f1d54f4d03a6b0a128a197a56
Hyunsu Joo <hs9880@gmail.com>
2021-10-07 11:47:08 +0900

    i-1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Seo from '../components/Seo';
 import PostPreview from '../components/PostPreview';
 import Bio from '../components/Bio';
 const IndexPage = () => {
-    const posts = usePosts();
+    const posts = usePosts();aag
     return (
         <Layout>
             <Seo title="Home" description="" />;
