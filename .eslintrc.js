c900f3bb847dbde726a225d48e945ad2c6f1fcb2
Hyunsu Joo <hs9880@gmail.com>
2021-10-09 00:43:50 +0900

    :package: Configure husky,staged_lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,40 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    // parser: 'babel-parser',
-    plugins: ['@typescript-eslint', 'react', 'jsx-a11y'],
-    env: {
-        browser: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:jsx-a11y/recommended',
-        'plugin:prettier/recommended',
-        'prettier',
-    ],
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/no-unescaped-entities': [
+      'error',
+      {
+        forbid: [
+          { char: '>', alternatives: ['&gt;'] },
+          { char: '}', alternatives: ['&#125;'] }
+        ]
+      }
+    ]
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  }
 };
