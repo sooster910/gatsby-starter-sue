1fe6e2d018037626723da18677932dad70f6dd06
Hyunsu <hs9880@gmail.com>
2023-11-15 00:42:24 +0900

    [remove] acl configuration

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,4 +2,4 @@
 AWS_BUCKET_NAME=$(grep AWS_BUCKET_NAME .env | cut -d '=' -f2);
 
 # Clean the cache, run build and deploy the public directory and exclude the uploads folder.
-npm run clean && npm run build && aws s3 sync public s3://$AWS_BUCKET_NAME/ --exclude "folder-name-you-want-to-exclude" --acl public-read;
+npm run clean && npm run build && aws s3 sync public s3://$AWS_BUCKET_NAME/ --exclude "folder-name-you-want-to-exclude";
